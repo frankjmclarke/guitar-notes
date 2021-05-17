6a44744a72cf1c8816ae6398a5fbb6af800e88ca
Frank Clarke <fclarke@fclarke.com>
2021-05-16 23:27:31 -0700

    week timer added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 final Color darkBlue = Color.fromARGB(255, 18, 32, 47);
 
@@ -9,33 +10,80 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final width = MediaQuery.of(context).size.width;
-    final isSmall = width < 950;
-    return MaterialApp(
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: darkBlue),
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        // Outer white container with padding
-        body: Container(
-          color: Colors.white,
-          padding: EdgeInsets.symmetric(horizontal: 40, vertical: 80),
-          // Inner yellow container
-          child: Container(
-            // pass double.infinity to prevent shrinking of the painter area to 0.
-            width: width * 0.99995,
-            height: double.infinity,
-            color: Colors.white60,
-            child: CustomPaint(painter: FaceOutlinePainter()),
-          ),
+    return new MaterialApp(
+      title: 'SharedPreferences Demo',
+      home: new SharedPreferencesDemo(),
+    );
+  }
+}
+
+class SharedPreferencesDemo extends StatefulWidget {
+  SharedPreferencesDemo({Key? key}) : super(key: key);
+
+  @override
+  SharedPreferencesDemoState createState() => new SharedPreferencesDemoState();
+}
+
+class SharedPreferencesDemoState extends State<SharedPreferencesDemo> {
+  int firstDay = 0;
+  Future<SharedPreferences> _sprefs = SharedPreferences.getInstance();
+
+  Future<Null> getData() async {
+    final SharedPreferences prefs = await _sprefs;
+    int data = prefs.getInt('firstDay') ?? 8;
+    this.setState(() {
+      firstDay = data;
+    });
+  }
+
+  Future<Null> incrementCounter() async {
+    final SharedPreferences prefs = await _sprefs;
+    setState(() {
+      firstDay = firstDay + 1;
+      prefs.setString('firstDay', firstDay.toString());
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+//      child:new Text(counter.toString())
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: const Text("SharedPreferences Demo"),
+      ),
+      body: new Center(
+        child: Container(
+          // pass double.infinity to prevent shrinking of the painter area to 0.
+          width: double.infinity,
+          height: double.infinity,
+          color: Colors.white60,
+          child: CustomPaint(painter: FaceOutlinePainter(firstDay)),
         ),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
       ),
     );
   }
 }
 
 class FaceOutlinePainter extends CustomPainter {
+  int firstDay = 0;
+
+  FaceOutlinePainter(int firstDay) {
+    this.firstDay = firstDay;
+  }
+
   @override
-  void paint(Canvas canvas, Size size) {
+  Future<void> paint(Canvas canvas, Size size) async {
     final paint = Paint()
       ..style = PaintingStyle.stroke
       ..strokeWidth = 4.0
@@ -48,11 +96,9 @@ class FaceOutlinePainter extends CustomPainter {
     drawStrings(canvas, squareSize, paint);
 
     drawMarkers(squareSize, paint, canvas);
-
+    var week = dayDiff(firstDay);
     for (double across = 0; across < 6; across++)
-      drawCircles(across, squareSize, paint, canvas);
-
-
+      drawCircles(across, squareSize, paint, canvas, week);
   }
 
   @override
@@ -60,39 +106,24 @@ class FaceOutlinePainter extends CustomPainter {
 }
 
 void drawFrets(canvas, squareSize, paint) {
-
-  double half= squareSize/2;
+  double half = squareSize / 2;
   paint.color = Colors.cyan;
-  for (double down = 0; down < 13; down++) { //
-    canvas.drawLine(Offset(half, down* squareSize +half), Offset(squareSize*5.5, down* squareSize+half), paint);
+  for (double down = 0; down < 13; down++) {
+    //
+    canvas.drawLine(Offset(half, down * squareSize + half),
+        Offset(squareSize * 5.5, down * squareSize + half), paint);
   }
 }
 
 void drawStrings(canvas, squareSize, paint) {
-
-  double half= squareSize/2;
+  double half = squareSize / 2;
   paint.color = Colors.amberAccent;
   for (double across = 0; across < 6; across++)
-    canvas.drawLine(Offset( across* squareSize +half,half), Offset( across* squareSize+half,squareSize*12.5), paint);
+    canvas.drawLine(Offset(across * squareSize + half, half),
+        Offset(across * squareSize + half, squareSize * 12.5), paint);
 }
 
-void drawRect(canvas, squareSize, paint) {
-  double fontsize = squareSize / 1.5;
-
-  for (double down = 0; down < 12; down++) {
-    for (double across = 0; across < 5; across++) {
-      canvas.drawRRect(
-        RRect.fromRectAndRadius(
-            Rect.fromLTWH(squareSize * across + (squareSize / 2),
-                (down * squareSize) + (squareSize / 2), squareSize, squareSize),
-            Radius.circular(0)),
-        paint,
-      );
-    }
-  }
-}
-
-void drawCircles(across, squareSize, paint, canvas) {
+void drawCircles(across, squareSize, paint, canvas, week) {
   final data = [
     [5, 0, 7, 2, 10, 5],
     [7, 2, 9, 4, 0, 7],
@@ -102,7 +133,6 @@ void drawCircles(across, squareSize, paint, canvas) {
     [1, 8, 3, 10, 6, 1],
     [3, 10, 5, 0, 8, 3],
   ];
-  int down = data[5][across.round()];
   paint.style = PaintingStyle.fill;
   double fontsize = squareSize / 1.3;
   final textStyle = TextStyle(
@@ -114,8 +144,9 @@ void drawCircles(across, squareSize, paint, canvas) {
     fontSize: fontsize,
   );
 
-
   paint.color = Colors.red;
+
+  int down = data[week][across.round()];
   canvas.drawOval(
     Rect.fromLTWH(
         squareSize * across, squareSize * down, squareSize, squareSize),
@@ -127,12 +158,17 @@ void drawCircles(across, squareSize, paint, canvas) {
       text: span2, textAlign: TextAlign.left, textDirection: TextDirection.ltr);
   tp.text = span2;
   tp.layout();
+
   if (down < 10)
-    tp.paint(canvas,
-        new Offset(squareSize * across + (squareSize / 4), squareSize * down +squareSize/15));
+    tp.paint(
+        canvas,
+        new Offset(squareSize * across + (squareSize / 4),
+            squareSize * down + squareSize / 15));
   else
-    tp.paint(canvas,
-        new Offset(squareSize * across + (squareSize / 35), squareSize * down +squareSize/15));
+    tp.paint(
+        canvas,
+        new Offset(squareSize * across + (squareSize / 35),
+            squareSize * down + squareSize / 15));
   paint.style = PaintingStyle.stroke;
 }
 
@@ -140,7 +176,7 @@ void drawMarkers(squareSize, paint, canvas) {
   final data = [3, 5, 7, 9, 12];
   paint.color = Colors.black;
   paint.style = PaintingStyle.fill;
-  for (int i = 0; i < data.length-1; i++) {
+  for (int i = 0; i < data.length - 1; i++) {
     canvas.drawOval(
       Rect.fromLTWH(squareSize * 2.75, squareSize * (data[i] - 0.25),
           squareSize / 2, squareSize / 2),
@@ -148,13 +184,19 @@ void drawMarkers(squareSize, paint, canvas) {
     );
   }
   canvas.drawOval(
-    Rect.fromLTWH(squareSize * 1.75, squareSize * (data[data.length-1] - 0.25),
-        squareSize / 2, squareSize / 2),
+    Rect.fromLTWH(
+        squareSize * 1.75,
+        squareSize * (data[data.length - 1] - 0.25),
+        squareSize / 2,
+        squareSize / 2),
     paint,
   );
   canvas.drawOval(
-    Rect.fromLTWH(squareSize * 3.75, squareSize * (data[data.length-1] - 0.25),
-        squareSize / 2, squareSize / 2),
+    Rect.fromLTWH(
+        squareSize * 3.75,
+        squareSize * (data[data.length - 1] - 0.25),
+        squareSize / 2,
+        squareSize / 2),
     paint,
   );
   paint.style = PaintingStyle.stroke;
@@ -172,4 +214,29 @@ double getScreenHeightSafe(BuildContext context) {
 // Height (without SafeArea)
   var padding = MediaQuery.of(context).padding;
   return getScreenHeight(context) - padding.top - padding.bottom;
+}
+
+int weekDiff(int firstDay) {
+  var now = DateTime.now();
+  var hours = now.millisecondsSinceEpoch;
+  int diff = firstDay - hours;
+
+  diff = diff ~/ (1000 * 60 * 60); //minutes
+  diff = diff ~/ (24 * 7); //weeks
+  diff %= 7;
+
+  return diff;
+}
+
+int dayDiff(int firstDay) {
+  var now = DateTime.now();
+  var hours = now.millisecondsSinceEpoch;
+  int diff = firstDay - hours;
+
+  diff = diff ~/ (1000 * 60 * 60); //minutes
+  diff = diff ~/ (24); //days
+  diff %= 7;
+  print(diff);
+
+  return diff;
 }
